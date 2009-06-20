ea2add94084f7b69f3023489cd5c833a2f58224a
Sandro Turriate <sandro.turriate@gmail.com>
2009-06-20 02:45:46 -0400

    Add integration tests with fakeweb

--- spec/metamuse_spec.rb
+++ spec/metamuse_spec.rb
@@ -1,7 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Metamuse" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "integration" do
+    it "finds coldplay" do
+      FakeWeb.register_uri('http://developer.echonest.com/api/search_artists?query=coldplay&version=3&api_key=TEST', :file => web_fixture('echonest_coldplay_search.xml'))
+      Metamuse.echonest_api_key = "TEST"
+      Metamuse.find_artist('coldplay').name.should == 'Coldplay'
+    end
+  end
+
+  describe "finding an artist" do
+    it "searches through echonest" do
+      Metamuse::Services::Echonest.should_receive(:find_artist).with('coldplay')
+      Metamuse.find_artist('coldplay')
+    end
+  end
+
+  describe "finding tracks" do
+
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 require 'spec'
+begin; require 'rubygems'; rescue LoadError; end
+require 'fake_web'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'metamuse'
+
+def web_fixture(filename)
+  File.join(File.dirname(__FILE__), %W[web_fixtures #{filename}])
+end
+FakeWeb.allow_net_connect = false
+
