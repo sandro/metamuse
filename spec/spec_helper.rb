ac9395bab66c9b94f1ef16900104f5ca78b2b41b
Sandro Turriate <sandro.turriate@gmail.com>
2009-06-29 23:22:39 -0400

    Run spec from ruby command line

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
-begin; require 'rubygems'; rescue LoadError; end
-require 'spec'
-require 'spec/autorun'
-require 'fake_web'
-
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'metamuse'
+require 'spec'
+require 'spec/autorun'
 
 def web_fixture(filename)
   File.join(File.dirname(__FILE__), %W[web_fixtures #{filename}])
