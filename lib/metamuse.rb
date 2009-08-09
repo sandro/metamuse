b7d2fd42b6c32d8a3c7024fa1900996de5b302c7
Sandro Turriate <sandro.turriate@gmail.com>
2009-08-09 14:58:02 -0400

    Fix crack's date regex parser

--- lib/metamuse.rb
+++ lib/metamuse.rb
@@ -21,6 +21,8 @@ require 'metamuse/services/lastfm'
 require 'metamuse/services/lastfm/image'
 require 'metamuse/services/lastfm/artist'
 
+Crack::JSON::DATE_REGEX = /^\d{4}-\d{2}-\d{2}$|^\d{4}-\d{1,2}-\d{1,2}[ \t]+\d{1,2}:\d{2}:\d{2}(\.[0-9]*)?(([ \t]*)Z|[-+]\d{2}?(:\d{2})?)?$/
+
 module Metamuse
   autoload :Artist, 'metamuse/artist'
   autoload :Album, 'metamuse/album'
